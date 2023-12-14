dcadf5fdd696d179d390b086b5e816baf42da112
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-12-14 08:53:25 +0530

    Uncomment the code and learn Jest.

--- sum.test.js
+++ sum.test.js
@@ -85,11 +85,26 @@ const fetchData = require("./sum")
 // mockCallback(1)
 
 // => Test Mode
-test("mock implementation of a basic function",()=>{
-    const mock = jest.fn(x => 42  + x)
-    expect(mock(1)).toBe(43);
-    expect(mock).toHaveBeenCalledWith(1)
+// test("mock implementation of a basic function",()=>{
+//     const mock = jest.fn(x => 42  + x)
+//     expect(mock(1)).toBe(43);
+//     expect(mock).toHaveBeenCalledWith(1)
 
-})
+// })
+
+
+11. //Spies
+// test('spying on a method of an object',()=>{
+//     const video ={
+//         play(){
+//             return true
+//         }
+//     }
+//     const spy = jest.spyOn(video,'play');
+//     video.play()
+//     expect(spy).toHaveBeenCalled()
+//     spy.mockRestore()
+
+// })
 
 
