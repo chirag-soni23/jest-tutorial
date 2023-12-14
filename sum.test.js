e6771fac291aac1721489407c69d625dcf075a18
Chirag soni <129665526+chirag-soni23@users.noreply.github.com>
2023-12-14 08:46:29 +0530

    test

--- sum.test.js
+++ sum.test.js
@@ -86,6 +86,9 @@ const fetchData = require("./sum")
 
 // => Test Mode
 test("mock implementation of a basic function",()=>{
+    const mock = jest.fn(x => 42  + x)
+    expect(mock(1)).toBe(43);
+    expect(mock).toHaveBeenCalledWith(1)
 
 })
 
